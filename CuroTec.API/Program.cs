8b7c68ed9fe608f127c390a14416c4ba3b46b40f
Lucas Souza <lucasolv.souza@gmail.com>
2025-03-31 18:48:39 -0300

    migrations

--- CuroTec.API/Program.cs
+++ CuroTec.API/Program.cs
@@ -15,7 +15,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<CuroTecContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+        x => x.MigrationsAssembly("CuroTec.API")));
 
 builder.Services.AddScoped<IVehicleRepository, VehicleRepository>();
 
